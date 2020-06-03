b1924ed28ac8ae144afccffc043bb53e964fe23c
Manab <46393531+manabsaha@users.noreply.github.com>
2020-06-03 23:41:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask,render_template
 app = Flask(__name__) 
   
 @app.route('/',methods=['GET']) 
-def index(): 
+def index():
     return render_template('manabsaha.html')
   
 if __name__ == '__main__': 
